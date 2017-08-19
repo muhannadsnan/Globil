f05f9834fe3dc3820fb388a0ec1287f30185f205
Muhannad <msn-23@live.com>
2017-08-19 12:22:26 +0200

    8. aug: ++FIXES my wishlist page

--- resources/views/wishlists/show.blade.php
+++ resources/views/wishlists/show.blade.php
@@ -16,41 +16,35 @@
 					
 					<div class="wish-list-post  col-xs-12">
 						<div class="col-xs-9">
-							<a href="/cars/{{$wish->car->id}}" class="">
-							<div class="hd">
-								{{ $wish->car->brand }}, 
-								{{ $wish->car->model }},
-								{{ $wish->car->year }}
-							</div>
-						</a> 
-								
-						<span class="price text-left">{{ $wish->car->price . ' NOK' }}</span>
-						<span class="kilometer text-right">{{ $wish->car->kilometer }}</span>
-
-						<p>
-							Posted By
-							<a href="#">
-								<strong>{{ $wish->user->name }}</strong>
+							<a href="/cars/{{$wish->car->id}}" class="hd">
+									{{ $wish->car->brandSubdata() }}, 
+									{{ $wish->car->modelSubdata() }},
+									{{ $wish->car->year }}
 							</a>
-							 {{ $wish->car->created_at->diffForHumans() }}
-						</p>
 
-						<div class="caption">
-							{{ substr($wish->car->desc, 0, 50) }}.. <br/>
-						</div>
+							<span class="price text-left">{{ $wish->car->price . ' NOK' }}</span>
+							<span class="kilometer text-right">{{ $wish->car->kilometer . 'Kilometer' }}</span>
+
+							<p>
+								Posted By <a href="#"> <strong>{{ $wish->user->name }}</strong> </a>
+								{{ $wish->car->created_at->diffForHumans() }}
+							</p>
+
+							<div class="caption">
+								{{ substr($wish->car->desc, 0, 50) }}.. <br/>
+							</div>
 						</div>
 
 						<div class="thumbnail text-right col-xs-3">
-						<?php 
+							<?php
 							$pic0_id = @count($wish->car->pictures[0]->id) ? $wish->car->pictures[0]->id : 'no-image';
 							$pic0_ext = @count($wish->car->pictures[0]->id) ? $wish->car->pictures[0]->ext : 'png';
-						?>
+							?>
 							<img src="{{ asset('storage/images'.'/'. $pic0_id .'.'. $pic0_ext) }}" />
 						</div>
 					</div>
-					<hr>
-					
-					@endforeach
+					<hr>					
+				@endforeach
 
 			@else
 				<label>No cars in wish list.</label>
